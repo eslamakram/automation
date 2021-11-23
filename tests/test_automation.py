4da44f952acc24874c08072ddc0a3115f5590400
Eslam Akram <eslam_akram91@yahoo.com>
2021-11-23 12:21:38 +0200

    done

--- tests/test_automation.py
+++ tests/test_automation.py
@@ -1,5 +1,24 @@
 from automation import __version__
 
 
+from automation.automation import *
+import re
+
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_Emails():
+    with open('assets/emails.txt','r') as emails:
+        emails=emails.read()
+        emails=emails.split()
+    for email in emails:
+        match = re.search(r'[\w.+-]+@[\w-]+\.[\w.-]+', email)
+        assert bool(match)
+
+def test_Phone_Numbers():
+    with open('assets/phone_numbers.txt','r') as phones:
+        phones=phones.read()
+        phones=phones.split()
+    for phone in phones:
+        match = re.search(r'\d{3}-\d{3}-\d{4}$', phone)
+        assert bool(match)
